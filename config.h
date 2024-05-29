57ecb2ffeb188909eca7c116bde35a9a12b1057a
SpreadZhao <www.spreadzhao@outlook.com>
2024-05-29 14:02:41 +0800

    qq tile

--- config.h
+++ config.h
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ NULL,       "qq",       NULL,       1 << 4,       1,           -1 },
+	{ NULL,       "qq",       NULL,       1 << 4,       0,           -1 },
 	{ NULL,       "edge",     NULL,       1 << 1,       0,           -1 },
 	{ NULL,       "peek",     NULL,       0,            1,           -1 }
 };
